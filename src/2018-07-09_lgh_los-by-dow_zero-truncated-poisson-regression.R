cb9e84c02edb2d6dbffe30a34ad8fc672f9bb650
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-07-09 16:12:37 -0700

    model m3.los.vs.dow using zero-truncated Poisson regression

--- src/2018-07-09_lgh_los-by-dow_zero-truncated-poisson-regression.R
+++ src/2018-07-09_lgh_los-by-dow_zero-truncated-poisson-regression.R
@@ -18,6 +18,9 @@ library("VGAM")
 #****************
 
 
+# reference: https://stats.idre.ucla.edu/r/dae/zero-truncated-poisson/ 
+
+
 # 0) read in data: ---------------
 if(!exists("df1.raw.data")){
     source(here("src",
@@ -26,5 +29,34 @@ if(!exists("df1.raw.data")){
 
 str(df1.raw.data)
 summary(df1.raw.data)
+
+
+
+
+# 1) regression of LOSDays: -----------
+# > 1st model: los ~ dow: --------
+
+m3.los.vs.dow <- vglm(losdays ~ dow, 
+                      family = pospoisson(), 
+                      data = df1.raw.data)
+
+summary(m3.los.vs.dow)
+
+# note: broom can't deal with vglm objects 
+
+
+# >> residual diagnostics: ----
+m3.output <- data.frame(resid = resid(m3.los.vs.dow),
+                        fitted = fitted(m3.los.vs.dow))
+
+p9.resid.vs.fitted <- 
+    ggplot(m3.output, 
+           aes(fitted, resid)) +
+    geom_jitter(position=position_jitter(width=.25), alpha=.5) +
+    stat_smooth(method="loess"); p9.resid.vs.fitted
+
+# mean is about 0 across all fitted values ==> that's good!
+
+
 
 
