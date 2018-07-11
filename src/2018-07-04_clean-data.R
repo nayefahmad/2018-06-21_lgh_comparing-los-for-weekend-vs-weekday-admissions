29a4267b34f8ea7985e1d8965fe0f08eafb9bc67
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-07-10 22:15:50 -0700

    read in test data

--- src/2018-07-04_clean-data.R
+++ src/2018-07-04_clean-data.R
@@ -19,7 +19,7 @@ library("ggpubr")
 # > use zero-truncated Poisson regression?: https://stats.idre.ucla.edu/r/dae/zero-truncated-poisson/
 #****************
 
-# 0) read, prep data: ------------------
+# 0) read, prep training data: ------------------
 df1.raw.data <- read_csv(here("results", 
                               "output from src", 
                               "2018-06-21_lgh_comparing-los-for-weekend-vs-weekday-admissions.csv"), 
@@ -51,8 +51,44 @@ summary(df1.raw.data)
 summary(df1.raw.data$unit.code)
 
 
+# 1) read, prep test data: ------------------
+df2.test.data <- read_csv(here("results", 
+                               "output from src", 
+                               "2018-07-10_lgh_los-test-data.csv"), 
+                          na = "NULL")
 
-# 4) WRITE OUTPUTS: ------------------
+names(df2.test.data) <- tolower(names(df2.test.data))
+
+df2.test.data %<>% 
+    rename(site = admissionfacilitylongname, 
+           date = adjustedadmissiondate, 
+           dow = day_of_week, 
+           age = admissionage, 
+           unit.code = admissionnursingunitcode) %>% 
+    mutate(date = as.Date(date, '%m/%d/%Y'), 
+           ishomeless = as.factor(ishomeless), 
+           dow = fct_relevel(dow, 
+                             "Monday", 
+                             "Tuesday", 
+                             "Wednesday", 
+                             "Thursday",
+                             "Friday", 
+                             "Saturday", 
+                             "Sunday")) %>% 
+    mutate_if(is.character, factor) %>% 
+    select(-admissionnursingunit)
+
+
+str(df2.test.data)
+summary(df2.test.data)
+summary(df2.test.data$unit.code)
+
+
+
+
+
+
+# 3) WRITE OUTPUTS: ------------------
 write_csv(df1.raw.data, 
           here("results",
                "output from src", 
