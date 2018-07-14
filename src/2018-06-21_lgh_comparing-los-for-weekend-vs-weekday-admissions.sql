237c4798b47b74a4ace10c34152908c7f5df2d53
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-07-14 01:21:16 -0700

    sql: add gender, years from 2009, patient service

--- src/2018-06-21_lgh_comparing-los-for-weekend-vs-weekday-admissions.sql
+++ src/2018-06-21_lgh_comparing-los-for-weekend-vs-weekday-admissions.sql
@@ -5,19 +5,22 @@
 -- Created: 2018-06-21
 
 SELECT [AdmissionFacilityLongName]
-	  , AdjustedAdmissionDate
-	  , Datename(dw, AdjustedAdmissionDate) as day_of_week
+	  ,[AdjustedAdmissionDate]
+	  ,(year([AdjustedAdmissionDate]) - 2009) as yearsFrom2009
+	  ,Datename(dw, AdjustedAdmissionDate) as day_of_week
       ,[PatientId] --
       ,[AdmissionAge] -- 
+	  ,[Gender]
       ,[IsHomeless] -- 
       ,[AdmissionNursingUnitCode] -- 
 	  ,[AdmissionNursingUnit] 
+	  ,[AdmissionPatientServiceCode]
       ,[LOSDays] --
 
 FROM [ADTCMart].[ADTC].[AdmissionDischargeView]
   
 where AdmissionFacilityLongName = 'Lions Gate Hospital' 
-	and AdjustedAdmissionDate between '2017-01-01' and '2017-02-28' 
+	and AdjustedAdmissionDate between '2009-01-01' and '2017-02-28' 
 	and AdmissionNursingUnitCode not in ('EIP', 'N/A', 'en1', 'en2', 'nsy', 'nsh', '3po', 'dcs', 'es1', 'es2', 'es3', 'end', 'nsh ssh' , 'scn') 
 	--and AdmissionNursingUnitCode  = '4e' 
 
